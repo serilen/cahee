6ab731c40015b5c31d428670a2451bb4d212cd56
serilen <serilen29@gmail.com>
2022-01-17 00:27:59 +0200

    Add adaptive menu

--- js/main.js
+++ js/main.js
@@ -1,44 +1,86 @@
 ;(function(){
 	"use strict";
+//Проверка на устройство пользователя
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android()
+			|| isMobile.BlackBerry()
+			|| isMobile.iOS()
+			|| isMobile.Opera()
+			|| isMobile.Windows()
+			);
+	}
+};
 
-	$(document).ready(function() {
+//Если пользователь использует тачскрин, то показываем стрелочку для вложенного меню
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+	let menuArrows = document.querySelectorAll(".menu__arrow");
+		//Проверка на существование элементов
+		if (menuArrows.length > 0) {
+			for (let i = 0; i < menuArrows.length; i++) {
+				const menuArrow = menuArrows[i];
 
-		$("#main_menu").on("click","a", function (event) {
+				menuArrow.addEventListener('click', function(event) {
+					menuArrow.parentElement.classList.toggle('_active');
+				})
+			}
+		}
+	}
+	else {
+		document.body.classList.add('_pc');
+	}
+
+// Меню бургер
+
+const burgerMenu = document.querySelector(".menu__burger-button");
+const bodyMenu = document.querySelector(".menu__body");
+//Проверка на существование элементов
+if (burgerMenu) {
+	burgerMenu.addEventListener('click', function(event) {
+		document.body.classList.toggle('locked');
+		burgerMenu.classList.toggle('_active');
+		bodyMenu.classList.toggle('_active');
+	})
+}
+
+$(document).ready(function() {
+//Плавная прокрутка к якорю
+$("body").on("click","a", function (event) {
+
+//При нажатии на ссылку убираем экран меню
+	if ($('.menu__burger-button').hasClass('_active')) {
+		$('.menu__body').removeClass('_active');
+		$('.menu__burger-button').removeClass('_active');
+		$('body').removeClass('locked');
+	};
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
         //забираем идентификатор бока с атрибута href
         let id  = $(this).attr('href'),
         //узнаем высоту от начала страницы до блока на который ссылается якорь
         top = $(id).offset().top;
-        //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({scrollTop: top}, 1500);
+        //узнаем высоту меню от начала страницы
+        let heightMenu = $(".header__wrap").outerHeight();
+        //анимируем переход на расстояние - (top-height) -  за 1500 мс
+        $('body,html').animate({scrollTop: (top - heightMenu)}, 1500);
      });
-
-		$(window).resize(function () {
-			let width = $('body').innerWidth()
-			if(width < 768) {
-				$("#button_burger_line").removeClass("burger-button__line--no-visibilite");
-				$("#button_burger").removeClass("burger-button--active");
-				$("#main_menu").addClass("sr-only").removeClass('main-menu--show');
-			} else {
-				$("#main_menu").removeClass("sr-only").removeClass('main-menu--show');
-				
-			}
-		})
-	});
-
-
-	const buttonBurger = document.querySelector('#button_burger');
-	const mainMenu = document.querySelector('#main_menu');
-	const buttonBurgerLine = document.querySelector('#button_burger_line');
-
-
-	buttonBurger.addEventListener('click', () => {
-		mainMenu.classList.toggle("sr-only");
-		mainMenu.classList.toggle("main-menu--show");
-		buttonBurgerLine.classList.toggle("burger-button__line--no-visibilite");
-		buttonBurger.classList.toggle("burger-button--active");
-	});
-	
+})
 
 })();
