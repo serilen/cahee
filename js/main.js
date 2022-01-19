a15b688fcc4af64598e53945f339e4f9838eb66c
serilen <serilen29@gmail.com>
2022-01-20 01:28:41 +0200

    fixed header apply BEM

--- js/main.js
+++ js/main.js
@@ -1,75 +1,25 @@
 ;(function(){
 	"use strict";
-//Проверка на устройство пользователя
-const isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	iOS: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return (
-			isMobile.Android()
-			|| isMobile.BlackBerry()
-			|| isMobile.iOS()
-			|| isMobile.Opera()
-			|| isMobile.Windows()
-			);
-	}
-};
-
-//Если пользователь использует тачскрин, то показываем стрелочку для вложенного меню
-if (isMobile.any()) {
-	document.body.classList.add('_touch');
-	let menuArrows = document.querySelectorAll(".menu__arrow");
-		//Проверка на существование элементов
-		if (menuArrows.length > 0) {
-			for (let i = 0; i < menuArrows.length; i++) {
-				const menuArrow = menuArrows[i];
-
-				menuArrow.addEventListener('click', function(event) {
-					menuArrow.parentElement.classList.toggle('_active');
-				})
-			}
-		}
-	}
-	else {
-		document.body.classList.add('_pc');
-	}
+	$(document).ready(function() {
 
 // Меню бургер
 
-const burgerMenu = document.querySelector(".menu__burger-button");
-const bodyMenu = document.querySelector(".menu__body");
-//Проверка на существование элементов
-if (burgerMenu) {
-	burgerMenu.addEventListener('click', function(event) {
-		document.body.classList.toggle('locked');
-		burgerMenu.classList.toggle('_active');
-		bodyMenu.classList.toggle('_active');
-	})
-}
+$("#burger_button").on('click', function(event) {
+	$('body').toggleClass('hidden');
+	$('#burger_button').toggleClass('burger-button--active');
+	$("#nav").toggleClass('nav--active');
+})
 
-$(document).ready(function() {
+
 //Плавная прокрутка к якорю
 $("body").on("click","a", function (event) {
 
 //При нажатии на ссылку убираем экран меню
-if ($('.menu__burger-button').hasClass('_active')) {
-	$('.menu__body').removeClass('_active');
-	$('.menu__burger-button').removeClass('_active');
-	$('body').removeClass('locked');
-};
+if ($("#burger_button").hasClass('burger-button--active')) {
+	$("#nav").removeClass('nav--active');
+	$("#burger_button").removeClass('burger-button--active');
+	$('body').removeClass('hidden');
+}
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
         //забираем идентификатор бока с атрибута href
@@ -80,11 +30,11 @@ if ($('.menu__burger-button').hasClass('_active')) {
         let heightMenu = $(".header__wrap").outerHeight();
         //анимируем переход на расстояние - (top-height) -  за 1500 мс
         $('body,html').animate({scrollTop: (top - heightMenu)}, 1500);
-        //убираем прозрачный фон у меню
+        //убираем прозрачный фон у меню при переходе по ссылке
         $('header').css('background-color', '#fff');
      });
 
-
+//Если скролим вниз меню делаем непрозрачным
 $(window).scroll(function() {
 	let scrolled = $(window).scrollTop(),
 	scrollPrev = 0;
@@ -96,6 +46,13 @@ $(window).scroll(function() {
 	}
 	scrollPrev = scrolled;
 });
+
+
+//стрелку подключаем на выпадающее меню
+$("#arrow").on("click", function (event) {
+	$('#sub_list').toggleClass("menu-list__sub-list--active");
+})
+
 
 })
 
